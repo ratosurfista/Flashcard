2a0e5e95d7685582295f71ee0f1fcfcb232081d3
ratosurfista <00001119348572sp@al.educacao.sp.gov.br>
2024-11-27 20:55:32 -0300

    Add files via upload

---
+++ Flashcard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'O primeiro presidente do Brasil foi Marechal Deodoro da Fonseca.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o planeta mais próximo do Sol?',
+    'O planeta mais próximo do Sol é Mercúrio.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 8 vezes 7?',
+    '8 vezes 7 é igual a 56.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'O maior oceano do mundo é o Oceano Pacífico.'
+)
